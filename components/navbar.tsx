80215a39cddff9968241eb67c756ca7c76f9cc20
Ludovic Mermod <ludovic.mermod@epfl.ch>
2023-01-09 16:57:27 +0100

    Add navbar stub

---
+++ components/navbar.tsx
@@ -0,0 +1,14 @@
+import Link from "next/link";
+
+export default function NavBar(){
+    return (
+        <div className="navbar">
+            <ul>
+                <li><Link href="">Home</Link></li>
+                <li><Link href="aboutme">About me</Link></li>
+                <li><Link href="projects">Projects</Link></li>
+                <li><Link href="socials">Socials</Link></li>
+            </ul>
+        </div>
+    )
+}
