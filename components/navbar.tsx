06c7d1a81fc85091cae3e22de091c548fd36f45b
Ludovic Mermod <ludovic.mermod@epfl.ch>
2023-01-11 11:54:01 +0100

    Fix path in links

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -4,10 +4,10 @@ export default function NavBar(){
     return (
         <div className="navbar">
             <ul>
-                <li><Link href="">Home</Link></li>
-                <li><Link href="aboutme">About me</Link></li>
-                <li><Link href="projects">Projects</Link></li>
-                <li><Link href="socials">Socials</Link></li>
+                <li><Link href="/">Home</Link></li>
+                <li><Link href="/aboutme">About me</Link></li>
+                <li><Link href="/projects">Projects</Link></li>
+                <li><Link href="/socials">Socials</Link></li>
             </ul>
         </div>
     )
