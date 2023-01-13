2d9b6c73fdde5c36232a56bec28c0e38af24f42c
Ludovic Mermod <ludovic.mermod@epfl.ch>
2023-01-13 19:57:49 +0100

    Add homepage style

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,7 @@ import type { AppProps } from 'next/app'
 import 'styles/global.css'
 import 'styles/navbar.css'
 import 'styles/aboutme.css'
+import 'styles/home.css'
 
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
