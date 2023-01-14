9749927056383e46e53ec805ebe8d653bb9682ba
Ludovic Mermod <ludovic.mermod@epfl.ch>
2023-01-14 17:57:35 +0100

    Fill projects page

--- pages/projects.tsx
+++ pages/projects.tsx
@@ -1,13 +1,40 @@
+import { readFileSync } from "fs";
 import Head from "next/head";
+import ProjectRow, { Project } from "../components/project_row";
 
-export default function Projects() {
-    return (
-      <>
-        <Head>      
-          <title>Projects - Ludovic Mermod</title>
-        </Head>
+export default function Projects({ projects }: { projects: Project[] }) {
+  return (
+    <>
+      <Head>
+        <title>Projects - Ludovic Mermod</title>
+      </Head>
+      <div id="projects">
         <p>My projects:</p>
-      </>
-    )
+
+        <table id="project-table">
+          <tbody>
+            <thead>
+              <tr>
+                <th key="name"><p>Name</p></th>
+                <th key="active"><p>Active</p></th>
+                <th key="description"><p>Description</p></th>
+                <th key="languages"><p>Languages</p></th>
+                <th key="organization"><p>Organization</p></th>
+                <th key="repository"><p>Repository</p></th>
+              </tr>
+            </thead>
+            {projects.map(p => <ProjectRow key={p.id} project={p} />)}
+          </tbody>
+        </table>
+      </div>
+    </>
+  )
+}
+
+export async function getStaticProps() {
+  return {
+    props: {
+      projects: JSON.parse(readFileSync('data/projects.json', 'utf8').toString())
+    }
   }
-  
+}
