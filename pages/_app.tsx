7fa33f339ace5631bf7d07b02ceefa660dfbe130
Ludovic Mermod <ludovic.mermod@epfl.ch>
2023-01-15 14:02:17 +0100

    Add projects page style

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,6 +3,7 @@ import 'styles/global.css'
 import 'styles/navbar.css'
 import 'styles/aboutme.css'
 import 'styles/home.css'
+import 'styles/projects.css'
 
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
