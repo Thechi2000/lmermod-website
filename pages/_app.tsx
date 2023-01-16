50bdaa96e5671d4333c0a9f9b7eddbc0efa3fc25
Ludovic Mermod <ludovic.mermod@epfl.ch>
2023-01-16 13:53:48 +0100

    Add competences page style

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,6 +4,7 @@ import 'styles/navbar.css'
 import 'styles/aboutme.css'
 import 'styles/home.css'
 import 'styles/projects.css'
+import 'styles/competences.css'
 
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
