2af825eb31ffd9514355daef8618b9b0eb57216e
Ludovic Mermod <ludovic.mermod@epfl.ch>
2023-01-16 13:53:29 +0100

    Add competences page

---
+++ pages/aboutme/competences.tsx
@@ -0,0 +1,28 @@
+import { readFileSync } from "fs";
+
+interface Language {
+    name: string,
+    years: number,
+    level: number,
+}
+
+export default function Languages({ languages }: { languages: Language[] }) {
+    return (
+        <div id="competences">
+            <h2>Competences</h2>
+            <p>Languages (with experience and expertise level)</p>
+            <ul>
+                {languages.sort((a, b) => b.years - a.years || b.level - a.level || a.name.localeCompare(b.name)).map(l => <li><p>{l.name}: {l.years == 0 ? '< 1' : l.years.toString()} years ({l.level}/5)</p></li>)}
+            </ul>
+        </div>
+    )
+}
+
+
+export async function getStaticProps() {
+    return {
+        props: {
+            languages: JSON.parse(readFileSync('data/languages.json', 'utf8').toString())
+        }
+    }
+}
