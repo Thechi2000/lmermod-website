3fc3df365eea2558b805d8de24dce2fd0289f096
Ludovic Mermod <ludovic.mermod@epfl.ch>
2023-01-20 10:59:32 +0100

    Add social page style

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,6 +5,7 @@ import 'styles/aboutme.css'
 import 'styles/home.css'
 import 'styles/projects.css'
 import 'styles/competences.css'
+import 'styles/socials.css'
 
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
