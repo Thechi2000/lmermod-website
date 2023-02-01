c4a96a04c9f3554a19cf3c2a127674a2a3b6bf89
Ludovic Mermod <ludovic.mermod@epfl.ch>
2023-02-01 12:19:55 +0100

    Move navbar to top

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -2,16 +2,22 @@ import Image from "next/image";
 import Link from "next/link";
 
 import logo from 'assets/logo.svg'
+import { forwardRef } from "react";
 
-export default function NavBar(){
-    return (
-        <div className="navbar">
-          <Image src={logo} alt="logo" style={{width: "80%", height: "fit-content", paddingBottom: "25%"}}/>
-          <Link href="/">Home</Link>
-          <Link href="/aboutme">About me</Link>
-          <Link href="/projects">Projects</Link>
-          <Link href="/socials">Socials</Link>
-          <Link href="/competences">Competences</Link>
-        </div>
-    )
+export default function NavBar() {
+  const Logo = forwardRef(({ onClick, href }, ref) => <a href={href} onClick={onClick} ref={ref}><Image src={logo} alt="logo" style={{ width: "80px", height: "80px", paddingRight: "40px" }} /></a>)
+
+
+  return (
+    <div className="navbar">
+      <Link href="/" passHref legacyBehavior><Logo /></Link>
+      <div id="navbar-links">
+        <Link href="/">Home</Link>
+        <Link href="/aboutme">About me</Link>
+        <Link href="/projects">Projects</Link>
+        <Link href="/socials">Socials</Link>
+        <Link href="/competences">Competences</Link>
+      </div>
+    </div>
+  )
 }
