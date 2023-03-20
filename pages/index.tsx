8feee1dbe292fedb9d0c9b0abe60b857eeba7e69
Ludovic Mermod <ludovic.mermod@epfl.ch>
2023-03-20 17:26:49 +0100

    improve main page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,22 +1,21 @@
 import Head from "next/head"
 import Image from 'next/image'
 
-import ferris from 'assets/ferris.svg'
+import ferris from 'assets/ferris-gesture.svg'
 
 export default function Home() {
   return (
     <>
-      <Head>      
+      <Head>
         <title>Home - Ludovic Mermod</title>
       </Head>
-      <div id="home" className="horizontal-container">
+      <div id="home" className="flex pt-8 space-x-10 ">
         <div>
-          <h1>Welcome to my website !</h1>
-          <h1>All hail the crabs !</h1>
-          <h2>By Ludovic Mermod</h2>
+          <h1>Welcome to my website&nbsp;!</h1>
+          <h2>Ludovic Mermod</h2>
         </div>
 
-        <Image id='ferris' src={ferris} alt='ferris' />
+          <Image id='ferris' src={ferris} alt='ferris' />
       </div>
     </>
   )
