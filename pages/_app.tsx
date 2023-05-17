1f970ae8414896a47204e38cc047f4d1a9fec426
Ludovic Mermod <ludovic.mermod@epfl.ch>
2023-05-17 16:39:21 +0200

    add navbar.css

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,11 @@
 import type { AppProps } from 'next/app'
 import 'styles/global.css'
 import 'styles/aboutme.css'
-import 'styles/home.css'
 import 'styles/projects.css'
 import 'styles/competences.css'
 import 'styles/socials.css'
+import 'styles/navbar.css'
+import 'styles/home.css'
 import NavBar from '../components/navbar'
 
 export default function App({ Component, pageProps }: AppProps) {
