6d123a066222551c296dcd095fb4abadada813dc
Ludovic Mermod <ludovic.mermod@epfl.ch>
2023-05-18 16:52:30 +0200

    add prettier

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,32 +1,28 @@
-import Link from 'next/link'
-import { useRouter } from 'next/router'
+import Link from "next/link";
+import { useRouter } from "next/router";
 
 const navigation = [
-  { name: 'Home', href: '/' },
-  { name: 'About me', href: '/aboutme' },
-  { name: 'Projects', href: '/projects' },
-  { name: 'Socials', href: '/socials' },
-  { name: 'Skills', href: '/skills' },
-]
+  { name: "Home", href: "/" },
+  { name: "About me", href: "/aboutme" },
+  { name: "Projects", href: "/projects" },
+  { name: "Socials", href: "/socials" },
+  { name: "Skills", href: "/skills" },
+];
 
 export default function NavBar() {
   const router = useRouter();
 
   return (
-    <div id='navbar'>
-        {navigation.map((item) => (
-          <Link
-            key={item.name}
-            href={item.href}
-            className={
-              (router.asPath == item.href ? 'current-navbar-item' : '') + ' navbar-item'
-            }
-          >
-            <p style={{ fontSize: '1.5em' }}>
-              {item.name}
-            </p>
-          </Link>
-        ))}
-      </div>
-  )
+    <div id="navbar">
+      {navigation.map((item) => (
+        <Link
+          key={item.name}
+          href={item.href}
+          className={(router.asPath == item.href ? "current-navbar-item" : "") + " navbar-item"}
+        >
+          <p style={{ fontSize: "1.5em" }}>{item.name}</p>
+        </Link>
+      ))}
+    </div>
+  );
 }
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,12 @@
-import type { AppProps } from 'next/app'
-import 'styles/global.css'
-import 'styles/aboutme.css'
-import 'styles/projects.css'
-import 'styles/competences.css'
-import 'styles/socials.css'
-import 'styles/navbar.css'
-import 'styles/home.css'
-import NavBar from '../components/navbar'
+import type { AppProps } from "next/app";
+import "styles/global.css";
+import "styles/aboutme.css";
+import "styles/projects.css";
+import "styles/competences.css";
+import "styles/socials.css";
+import "styles/navbar.css";
+import "styles/home.css";
+import NavBar from "../components/navbar";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
@@ -14,5 +14,5 @@ export default function App({ Component, pageProps }: AppProps) {
       <NavBar />
       <Component {...pageProps} />
     </div>
-  )
+  );
 }
