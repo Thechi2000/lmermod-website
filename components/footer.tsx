8986758ff718f599283d76df5de024734ec7104c
Ludovic Mermod <ludovic.mermod@epfl.ch>
2023-05-25 16:43:55 +0200

    fix links in footer

--- components/footer.tsx
+++ components/footer.tsx
@@ -7,28 +7,28 @@ import MailLogo from "assets/mail-logo.svg";
 export default function Footer() {
   return (
     <footer>
-      <a href="https://github.com/Thechi2000" target="_blank">
+      <a href="https://github.com/Thechi2000" target="_blank" rel="noopener noreferrer nofollow">
         <div className="footer-link-content">
           <GithubLogo fill={"var(--color)"} />
           Github
         </div>
       </a>
 
-      <a href="https://gitlab.com/Thechi2000" target="_blank">
+      <a href="https://gitlab.com/Thechi2000" target="_blank" rel="noopener noreferrer nofollow">
         <div className="footer-link-content">
           <GitlabLogo />
           Gitlab
         </div>
       </a>
 
-      <a href="https://t.me/lmermod" target="_blank">
+      <a href="https://t.me/lmermod" target="_blank" rel="noopener noreferrer nofollow">
         <div className="footer-link-content">
           <TelegramLogo />
           Telegram
         </div>
       </a>
 
-      <a href="mailto:ludovic.mermod@epfl.ch" target="_blank">
+      <a href="mailto:ludovic.mermod@epfl.ch" target="_blank" rel="noopener noreferrer nofollow">
         <div className="footer-link-content">
           <MailLogo fill={"var(--color)"} />
           Email
