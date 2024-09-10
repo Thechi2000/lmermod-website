6278b082546af0a78f0f3ec88124c8540d877a16
Ludovic Mermod <ludovic.mermod@epfl.ch>
2024-09-10 19:31:53 +0200

    feat: set default ordering

--- src/pages/projects.tsx
+++ src/pages/projects.tsx
@@ -24,7 +24,7 @@ export default function Projects({ projects }: { projects: Project[] }) {
     .reduce((acc, val) => (val != undefined && acc.includes(val) ? acc : [...acc, val as string]), [] as string[])
     .sort();
 
-  const [ordering, setOrdering] = useState(null as Ordering | null);
+  const [ordering, setOrdering] = useState({ column: "active", asc: true } as Ordering | null);
 
   function updateFilters(change: Partial<ProjectFilters>) {
     console.log(JSON.stringify(change));
